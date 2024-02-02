75c22d91e411344818a62a16f5440053b8e644a0
Tilde Holmqvist <tildeholmqvist@gmail.com>
2024-02-02 11:47:47 +0000

    adding the comment models

--- codestar/settings.py
+++ codestar/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['8000-tildeholmqvi-djangoblog-s55zo1rf90o.ws-eu107.gitpod.io','.herokuapp.com']
 
