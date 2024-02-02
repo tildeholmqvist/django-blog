46e645a93aa1c1c4603fa0879d8bf14266f78e13
Tilde Holmqvist <tildeholmqvist@gmail.com>
2024-02-02 11:53:02 +0000

    adding the migrations

--- blog/models.py
+++ blog/models.py
@@ -21,5 +21,5 @@ class Comment(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments" )
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="comments")
     body = models.TextField()
-    apporoved = models.BooleanFiels(default=False)
+    apporoved = models.BooleanField(default=False)
     created_on = models.DateTimeField(auto_now_add=True)
