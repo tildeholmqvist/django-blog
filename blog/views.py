91acb86b0c3ca0fe541bfdb29dab2711363137c6
Tilde Holmqvist <tildeholmqvist@gmail.com>
2024-02-05 12:58:50 +0000

    spelling check, bc i got error

--- blog/views.py
+++ blog/views.py
@@ -3,6 +3,8 @@ from django.views import generic
 from .models import Post
 
 # Create your views here.
+
+
 class PostList(generic.ListView):
     queryset = Post.objects.filter(status=1)
     template_name = "blog/index.html"
@@ -22,12 +24,6 @@ def post_detail(request, slug):
 
     :template:`blog/post_detail.html`
     """
-
     queryset = Post.objects.filter(status=1)
     post = get_object_or_404(queryset, slug=slug)
-
-    return render(
-        request,
-        "blog/post_detail.html",
-        {"post": post},
-    )
+    return render(request, "blog/post_detail.html", {"post": post},)
